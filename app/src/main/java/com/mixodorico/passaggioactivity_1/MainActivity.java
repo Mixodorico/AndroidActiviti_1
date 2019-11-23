5898d34b1cf0bb8c1b8eb3460b7b8d152d4d3a97
Passalenti Andrea <107227@aulecsit.uniud.it>
2019-11-23 18:56:11 +0100

    Inserimento bottoni e test

--- app/src/main/java/com/mixodorico/passaggioactivity_1/MainActivity.java
+++ app/src/main/java/com/mixodorico/passaggioactivity_1/MainActivity.java
@@ -1,14 +1,33 @@
 package com.mixodorico.passaggioactivity_1;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button);
+        button1.setOnClickListener(bottone1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
     }
+
+    // Create an anonymous implementation of OnClickListener
+    private View.OnClickListener bottone1 = new View.OnClickListener() {
+        public void onClick(View v) {
+            button2.setVisibility(View.VISIBLE);
+            button3.setVisibility(View.VISIBLE);
+        }
+    };
+
 }
