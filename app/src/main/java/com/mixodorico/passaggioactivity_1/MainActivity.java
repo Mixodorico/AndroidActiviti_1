bac3a821d38e7ca5fcefbd828125e49872a3fa02
Andrea <gilmour.david@hotmail.it>
2019-11-24 14:46:33 +0100

    Aggiunta ascoltatori al bottone1

--- app/src/main/java/com/mixodorico/passaggioactivity_1/MainActivity.java
+++ app/src/main/java/com/mixodorico/passaggioactivity_1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mixodorico.passaggioactivity_1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,23 +15,69 @@ public class MainActivity extends AppCompatActivity {
     private Button button2;
     private Button button3;
 
+    //Context context = getApplicationContext();
+    //final int duration = Toast.LENGTH_SHORT;
+
+    //Intent cambiaActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         button1 = (Button)findViewById(R.id.button);
-        button1.setOnClickListener(bottone1);
+        button1.setOnClickListener(simpleClick);
+        button1.setOnLongClickListener(longClick);
         button2 = (Button)findViewById(R.id.button2);
         button3 = (Button)findViewById(R.id.button3);
+
     }
 
+    //region Button1 Listeners
+
     // Create an anonymous implementation of OnClickListener
-    private View.OnClickListener bottone1 = new View.OnClickListener() {
+    private View.OnClickListener simpleClick = new View.OnClickListener() {
         public void onClick(View v) {
             button1.setText(R.string.button1_name2);
-            button2.setVisibility(View.VISIBLE);
-            button3.setVisibility(View.VISIBLE);
+            boolean enable = areEnabled();
+
+            if(!enable) {
+                button2.setVisibility(View.VISIBLE);
+                button3.setVisibility(View.VISIBLE);
+                }else{
+                button2.setVisibility(View.GONE);
+                button3.setVisibility(View.GONE);
+            }
+            button2.setEnabled(!enable);
+            button3.setEnabled(!enable);
         }
     };
 
+    // Create an anonymous implementation of OnLongClickListener
+    private View.OnLongClickListener longClick = new View.OnLongClickListener(){
+        public boolean onLongClick(View v){
+            button1.setText("Premuto a Lungo");
+            return true;
+        }
+    };
+    //endregion
+
+    // Create an anonymous implementation of OnClickListener
+    /*private View.OnClickListener simpleClickB2 = new View.OnClickListener() {
+        public void onClick(View v) {
+            CharSequence testoToast = context.getResources().getString(R.string.toast_button2); //R.string.toast_button2 restituisce "int"
+            Toast.makeText(context, testoToast, duration).show();
+
+            cambiaActivity = new Intent(context,SecondActivity.class);
+            //cambiaActivity.putExtra("username", account_username);
+            startActivity(cambiaActivity);
+        }
+    };*/
+
+    private boolean areEnabled(){
+        if(button2.isEnabled())   // posso controllare anche solo uno dei due
+            return true;
+        else
+            return false;
+    }
 }
