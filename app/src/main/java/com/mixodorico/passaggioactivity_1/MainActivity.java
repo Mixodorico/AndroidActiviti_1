58db3b402db6abce84693a482635ba4a7dbff988
Andrea <gilmour.david@hotmail.it>
2019-12-03 20:16:48 +0100

    Funzionamento Activity Secondarie

--- app/src/main/java/com/mixodorico/passaggioactivity_1/MainActivity.java
+++ app/src/main/java/com/mixodorico/passaggioactivity_1/MainActivity.java
@@ -15,22 +15,25 @@ public class MainActivity extends AppCompatActivity {
     private Button button2;
     private Button button3;
 
-    //Context context = getApplicationContext();
-    //final int duration = Toast.LENGTH_SHORT;
+    Context context;
+    final int duration = Toast.LENGTH_SHORT;
 
-    //Intent cambiaActivity;
+    Intent cambiaActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = getApplicationContext(); // il contesto va inizializzato all'interno dell'OnCreate
+
         button1 = (Button)findViewById(R.id.button);
         button1.setOnClickListener(simpleClick);
         button1.setOnLongClickListener(longClick);
         button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(simpleClickB2);
         button3 = (Button)findViewById(R.id.button3);
-
+        button3.setOnClickListener(simpleClickB3);
     }
 
     //region Button1 Listeners
@@ -63,16 +66,31 @@ public class MainActivity extends AppCompatActivity {
     //endregion
 
     // Create an anonymous implementation of OnClickListener
-    /*private View.OnClickListener simpleClickB2 = new View.OnClickListener() {
+    private View.OnClickListener simpleClickB2 = new View.OnClickListener() {
         public void onClick(View v) {
             CharSequence testoToast = context.getResources().getString(R.string.toast_button2); //R.string.toast_button2 restituisce "int"
             Toast.makeText(context, testoToast, duration).show();
 
-            cambiaActivity = new Intent(context,SecondActivity.class);
+            cambiaActivity = new Intent(context,Activity1.class);
             //cambiaActivity.putExtra("username", account_username);
             startActivity(cambiaActivity);
+
         }
-    };*/
+    };
+
+    // Create an anonymous implementation of OnClickListener
+    private View.OnClickListener simpleClickB3 = new View.OnClickListener() {
+        public void onClick(View v) {
+            CharSequence testoToast = context.getResources().getString(R.string.toast_button3); //R.string.toast_button2 restituisce "int"
+            Toast.makeText(context, testoToast, duration).show();
+
+            cambiaActivity = new Intent(context,Activity2.class);
+            //cambiaActivity.putExtra("username", account_username);
+            startActivity(cambiaActivity);
+
+        }
+    };
+
 
     private boolean areEnabled(){
         if(button2.isEnabled())   // posso controllare anche solo uno dei due
