8c1436e38f4437e324b4c6a774aa1925f4cd6692
Andrea <gilmour.david@hotmail.it>
2019-12-04 17:00:04 +0100

    Inserimento Connessione internet

--- app/src/main/java/com/mixodorico/passaggioactivity_1/Activity2.java
+++ app/src/main/java/com/mixodorico/passaggioactivity_1/Activity2.java
@@ -3,6 +3,7 @@ package com.mixodorico.passaggioactivity_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview); // definizione della connessione internet
+        myWebView.loadUrl("http://www.html.it");
+
+
     }
 }
