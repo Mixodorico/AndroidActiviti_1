a06153183c32c0816ed7ed26459500c3ac490a2b
Andrea <gilmour.david@hotmail.it>
2019-12-11 21:41:18 +0100

    Commentata autenticazione con Backend

--- app/src/main/java/com/mixodorico/passaggioactivity_1/Activity1.java
+++ app/src/main/java/com/mixodorico/passaggioactivity_1/Activity1.java
@@ -3,6 +3,7 @@ package com.mixodorico.passaggioactivity_1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,57 +25,151 @@ import com.google.android.gms.tasks.Task;
 
 public class Activity1 extends AppCompatActivity {
 
-    private static final String TAG = "Demo1";
+
+    private GoogleSignInOptions gso;
     private GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 9001;
-    Context context;
-    final int duration = Toast.LENGTH_SHORT;
+    private Context context;
 
+    private CallAPI callAPI;
+    final int duration = Toast.LENGTH_SHORT;
+    private static final String TAG = "Demo1";
+
+    //region CREATE,START
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
         context = getApplicationContext();
 
-
-        //getActionBar().setDisplayHomeAsUpEnabled(true); // occorre per inserire la freccia all'indietro. Basta pero' modificare il Manifest inserendo il parent
-
         //region SIGNIN GOOGLE
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        // Cofigurazione della richiesta di sign-in: e-mail, ID e info di base
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //.requestIdToken(getString(R.string.server_client_id)) // Per mandare un token al proprio server https
                 .requestEmail()
                 .build();
 
-        // Build a GoogleSignInClient with the options specified by gso.
+        // Definizione di un GoogleSignInClient con la configurazione del gso
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        // Set the dimensions of the sign-in button.
+        // Definizione della dimensone del bottone di SignIn
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
 
         signInButton.setOnClickListener(signInClick);
 
-        //findViewById(R.id.sign_in_button).setOnClickListener((View.OnClickListener) this);
         //endregion
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        //updateUI(account);
 
         /*
-        * - If GoogleSignIn.getLastSignedInAccount returns a GoogleSignInAccount object (rather than null), the user has already signed in to your app with Google.
-        * Update your UI accordingly—that is, hide the sign-in button, launch your main activity, or whatever is appropriate for your app.
-        * - If GoogleSignIn.getLastSignedInAccount returns null, the user has not yet signed in to your app with Google. Update your UI to display the Google Sign-in button.
+         * Nello Start si controlla l'esistenza di un Account fi Google SignIn e se l'utente e' gia' autenticato. Due casi:
+         * - Se GoogleSignIn.getLastSignedInAccount restituisce un GoogleSignInAccount object (invece di null), allora l'itente e' gia' loggato nell'app tramite Google.
+         * IN QUESTO CASO SI DEVE PROCEDERE AGGIORNANDO DIRETTAMENTE L'UI, LANCIANDO DIRETTAMENTE L'APPLICAZIONE SENZA MOSTRARE IL BOTTONE DI LOGIN.
+         * - Se restituisce null, allora l'utente deve ancora effettuare l'accesso. In questo caso si deve mostrare il bottone per il Signin
         */
+
+        // MODO 1 (Non silent Singin - in genere da usare se non funziona il Silent)
+        //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this); // da utilizzare se il metodo silent non funziona
+        //updateUI(account);
+
+        //MODO 2 (Silent SingIn)
+        /*
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient (context, gso);
+        googleSignInClient.silentSignIn ()
+                .addOnCompleteListener ((Activity) context, new OnCompleteListener<GoogleSignInAccount> () {
+
+                    @Override
+                    public void onComplete (@NonNull Task<GoogleSignInAccount> task) {
+                        handleSignInResult (task);
+                    }
+                });
+         */
+    }
+    //endregion
+
+    //region LISTENER + SIGNIN-OUT
+
+    private View.OnClickListener signInClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.sign_in_button:
+                    signIn();
+                    break;
+            }
+        }
+    };
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+    }
+    //endregion
+
+    // da usare se non si può effettuare col silent
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            //handleSignInResult(task);
+        }
+    }
+
+    /*
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String idToken = account.getIdToken();
+
+            //region INVIO ID AL SERVER TRAMITE HTTPS POST
+            callAPI = new CallAPI("492606205053-3i3unmv23a41ml8eiv8tk7c7lgcehj3k.apps.googleusercontent.com", idToken);
+            callAPI.doInBackground("","","");
+
+            //endregion
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+    */
+
+    private void updateUI(GoogleSignInAccount googleSignInAccount){
+        if(googleSignInAccount!=null){
+            Toast.makeText(context, R.string.oggettoNonNull, duration).show();
+            Toast.makeText(context, "Email: "+ googleSignInAccount.getEmail(), duration).show();
+            Toast.makeText(context, "ID: "+ googleSignInAccount.getId(), duration).show();
+            Toast.makeText(context, "ID_Token: "+ googleSignInAccount.getIdToken(), duration).show();
+        }else{
+            Toast.makeText(context, R.string.oggettoNull, duration).show();
+        }
+    }
+
+    // region MENU
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -95,69 +190,37 @@ public class Activity1 extends AppCompatActivity {
         }
         return false;
     }
-
-        private View.OnClickListener signInClick = new View.OnClickListener() {
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.sign_in_button:
-                    signIn();
-                    break;
-            }
-        }
-    };
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
-        }
-    }
-
-    private void updateUI(GoogleSignInAccount googleSignInAccount){
-        if(googleSignInAccount!=null){
-            Toast.makeText(context, R.string.oggettoNonNull, duration).show();
-            Toast.makeText(context, "Email: "+ googleSignInAccount.getEmail(), duration).show();
-            Toast.makeText(context, "ID: "+ googleSignInAccount.getId(), duration).show();
-            Toast.makeText(context, "ID_Token: "+ googleSignInAccount.getIdToken(), duration).show();
-        }else{
-            Toast.makeText(context, R.string.oggettoNull, duration).show();
-        }
-    }
-
-    private void signOut() {
-        mGoogleSignInClient.signOut()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
-    }
-
-
+    //endregion
 }
+
+/*
+Just add this in your dependencies
+
+compile 'org.apache.httpcomponents:httpcore:4.4.1'
+compile 'org.apache.httpcomponents:httpclient:4.5'
+
+Finally
+
+dependencies {
+compile fileTree(dir: 'libs', include: ['*.jar'])
+testCompile 'junit:junit:4.12'
+compile 'com.android.support:appcompat-v7:23.0.1'
+compile 'com.android.support:design:23.0.1'
+compile 'org.apache.httpcomponents:httpcore:4.4.1'
+compile 'org.apache.httpcomponents:httpclient:4.5'
+}
+
+And also add this code:
+
+ android {
+    useLibrary 'org.apache.http.legacy'
+         }
+*
+FYI
+
+Specify requirement for Apache HTTP Legacy library If your app is targeting API level 28 (Android 9.0) or above, you must include the following declaration within the element of AndroidManifest.xml.
+
+ <uses-library
+      android:name="org.apache.http.legacy"
+      android:required="false" />
+*/
---
+++ app/src/main/java/com/mixodorico/passaggioactivity_1/CallAPI.java
@@ -0,0 +1,50 @@
+package com.mixodorico.passaggioactivity_1;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CallAPI extends AsyncTask<String, String, Void> {
+
+    private String url;
+    private String token_ID;
+
+    public CallAPI(String url, String token_ID){
+        this.url=url;
+        this.token_ID=token_ID;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(String... params) {
+        String urlString = params[0]; // URL to call
+        String data = params[1]; //data to post
+        OutputStream out = null;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            out.close();
+
+            urlConnection.connect();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
